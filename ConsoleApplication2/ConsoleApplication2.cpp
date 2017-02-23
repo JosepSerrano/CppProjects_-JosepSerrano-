5cc5749368737d459fcb61ceb5890a01f8ebde67
josepserrano <josepserrano@PB1-06>
2017-02-23 19:21:16 +0100

    update

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,11 +1,57 @@
-// ConsoleApplication2.cpp : Defines the entry point for the console application.
-//
+#include <iostream>
+#include <string>
+#include <ctime>
 
-#include "stdafx.h"
+enum class Weapon { FISTS, GUN, SHOTGUN, REVOLVER, SNIPER, MACHINE_GUN, MAX };
+
+class Zombie;
+
+class Player {
+		
+	Weapon weapon;
+	float precision;
+	int life;
+	
 
 
-int main()
-{
-    return 0;
+	void attack(Zombie &z);
+
+	bool isAlive() {
+		return life > 0;
+	}
+
+	Player() {
+		weapon = static_cast<Weapon>( rand() % static_cast<int> (Weapon::MAX));
+		precision = (rand()%10)/10.0f;
+		life = rand() % 101;
+	}
+
+};
+
+class Zombie {
+	int distanceToPlayer;
+	float speed;
+	float damage;
+	int life;
+
+	void attack(Player &) {
+
+	}
+
+	bool isAlive() {
+		return life > 0;
+	}
+
+	Zombie() {
+		distanceToPlayer = (rand() % 181) + 20;
+		speed = (rand() % 210) / 10.0f;
+		damage = (rand() % 210) / 10.0f;
+		life = rand() % 101;
+	}
+};
+
+void Player::attack(Zombie &z) {
+
+	z::life -= weapon * precision;
+
 }
-
